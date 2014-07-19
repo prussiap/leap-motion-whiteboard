f0fe482c3a42bbcb1e04d7a6553276a116d536c3
ian-lickers <ijlickers@email.wm.edu>
2014-07-19 01:11:02 -0400

    Main Framework
    Code framework, to be extended allowing addons.

---
+++ MainFramework.java
@@ -0,0 +1,36 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFramework
+{
+	int loop = 1;
+	static int DEPTH = 50;
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		Sensor device = new Sensor();
+		Output panel = new Output(this);
+		boolean drawing;
+
+		while(loop == 1)
+		{
+			coords = device.getCoords();
+			if(coords[2] >= DEPTH)
+			{
+				drawing = true;
+			}
+			else
+			{
+				drawing = false;
+			}
+
+			panel.draw(coords, draw);
+			thread.sleep(1000);
+		}
+	}
+
+	public void quit()
+	{
+		loop = -1;
+	}
+}
